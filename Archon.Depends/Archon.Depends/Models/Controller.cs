50b11a3867d37a51bfde0ad6962a92ac2361cea1
Marcel Mata <marcel.mata@archoninfosys.com>
2014-02-17 19:23:25 +0000

    First version of first part of Archon.Depends

---
+++ Archon.Depends/Archon.Depends/Models/Controller.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Archon.Depends.Models
+{
+	public class Controller
+	{
+		public string Application { get; private set; }
+		public string FullyQualifiedTypeName { get; private set; }
+	}
+}
---
+++ Archon.Depends/Archon.Depends/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Archon.Depends
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			if (string.IsNullOrEmpty(args[0]))
+				throw new ApplicationException("need to provide DLL argument");
+
+			string dll = args[0];
+
+			Assembly assembly = Assembly.LoadFrom(dll);
+
+			var controllers = assembly.GetTypes()
+				.Where(a => !a.IsAbstract && !a.IsInterface && a.Name.EndsWith("Controller"));
+
+			int actions = 0;
+
+			foreach (var c in controllers)
+			{
+				Console.WriteLine(c.Name);
+
+				var methods = c.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly)
+					.Where(m => !m.IsSpecialName);
+
+				foreach (var a in methods)
+				{
+					Console.WriteLine("\t" + a.Name);
+				}
+
+				actions += methods.Count();
+			}
+
+			Console.WriteLine();
+			Console.WriteLine(args[0]);
+			Console.WriteLine("Controllers:" + controllers.Count());
+			Console.WriteLine("Actions:" + actions);
+		}
+	}
+}
